a46297b70cd41b54a335a10cd3421550e4a4e77c
Alok <alokgupta5100@gmail.com>
2024-01-15 20:07:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+
+function App(){
+
+  return (
+    <>
+    <Home />
+    <Footer />
+    <Header />
+    </>
+  )
+}
+
+export default App
